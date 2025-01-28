f2db01765c15215659d2557a0a9c63620675d0cd
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2025-01-28 17:07:08 +0100

    lombok fix

--- src/main/java/de/telran/bankapp/Client.java
+++ src/main/java/de/telran/bankapp/Client.java
@@ -1,6 +1,10 @@
 package de.telran.bankapp;
 
-//@AllArgsConstructor
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Client {
 
     private String id;
@@ -12,81 +16,4 @@ public class Client {
     private String phone;
     private ClientStatus status;
 
-    public Client() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getTaxCode() {
-        return taxCode;
-    }
-
-    public void setTaxCode(String taxCode) {
-        this.taxCode = taxCode;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public ClientStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ClientStatus status) {
-        this.status = status;
-    }
-
-    public Client(String id, String lastName, String firstName, String taxCode, String email, String address, String phone, ClientStatus status) {
-        this.id = id;
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.taxCode = taxCode;
-        this.email = email;
-        this.address = address;
-        this.phone = phone;
-        this.status = status;
-    }
 }
