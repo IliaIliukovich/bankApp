d11c3a08fead4c83f294f971ce5f524259ad9571
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2025-02-07 11:58:50 +0100

    Data layer added

--- src/main/java/de/telran/bankapp/repository/ClientRepository.java
+++ src/main/java/de/telran/bankapp/repository/ClientRepository.java
@@ -1,14 +1,10 @@
 package de.telran.bankapp.repository;
 
 import de.telran.bankapp.entity.Client;
-import de.telran.bankapp.entity.enums.ClientStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, String> {
