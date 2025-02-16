9324fc43b59cfc0cfc94c4002a4e5c411c363cc4
BiOksana <biokissme@gmail.com>
2025-02-16 20:01:35 +0100

    - Added validation for product status and amount and  for invalid product and client IDs - Created a new BankAppBadRequestException class

--- src/main/java/de/telran/bankapp/service/AccountService.java
+++ src/main/java/de/telran/bankapp/service/AccountService.java
@@ -4,10 +4,9 @@ import de.telran.bankapp.entity.Account;
 import de.telran.bankapp.entity.Agreement;
 import de.telran.bankapp.entity.Client;
 import de.telran.bankapp.entity.Product;
-import de.telran.bankapp.entity.enums.AccountStatus;
-import de.telran.bankapp.entity.enums.AccountType;
-import de.telran.bankapp.entity.enums.AgreementStatus;
-import de.telran.bankapp.entity.enums.CurrencyCode;
+import de.telran.bankapp.entity.enums.*;
+import de.telran.bankapp.exception.BankAppBadRequestException;
+import de.telran.bankapp.exception.BankAppResourceNotFoundException;
 import de.telran.bankapp.repository.AccountRepository;
 import de.telran.bankapp.repository.AgreementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,37 +50,40 @@ public class AccountService {
     }
 
 
-
     public Account create(Account account) {
         return repository.save(account);
 
     }
 
     public void deleteAccount(Long id) {
-       repository.deleteAccountById(id);
+        repository.deleteAccountById(id);
     }
 
 
     public Account createNewAccount(String clientId, Long productId, BigDecimal initialAmount) {
-        Optional<Product> product = productService.getProductById(productId);
+        Optional<Product> productOptional = productService.getProductById(productId);
         Optional<Client> optionalClient = clientService.getClientById(clientId);
-        Account account = null;
-        if (optionalClient.isPresent()) {
-            if (product.isPresent()) {
-                Product product1 = product.get();
-                account = new Account(null, "DE88370400440532013789", AccountType.CHECKING,
-                        AccountStatus.ACTIVE, initialAmount, product1.getCurrencyCode(), clientId);
-
-                Account saved = repository.save(account);
-                Agreement agreement = new Agreement(null, product1.getInterestRate(), AgreementStatus.ACTIVE,
-                        product1.getLimitAmount(), saved.getId(), productId);
-                agreementRepository.save(agreement);
-                return saved;
-            } else {
-
-            }
+        if (!optionalClient.isPresent()) {
+            throw new BankAppResourceNotFoundException("Client with id = " + clientId + " not found in database");
         }
-        return null;
+        if (!productOptional.isPresent()) {
+            throw new BankAppResourceNotFoundException("Product with id = " + productId + " not found in database");
+        }
+        Product product = productOptional.get();
+        if (product.getStatus() == ProductStatus.INACTIVE) {
+            throw new BankAppBadRequestException("The product status is INACTIVE");
+        }
+        if (initialAmount.compareTo(product.getLimitAmount()) > 0) {
+            throw new BankAppBadRequestException("InitialAmount is more than product limitAmount");
+        }
+        Account account = new Account(null, "DE88370400440532013789", AccountType.CHECKING,
+                AccountStatus.ACTIVE, initialAmount, product.getCurrencyCode(), clientId);
+
+        Account savedAccount = repository.save(account);
+        Agreement agreement = new Agreement(null, product.getInterestRate(), AgreementStatus.ACTIVE,
+                product.getLimitAmount(), savedAccount.getId(), productId);
+        agreementRepository.save(agreement);
+        return savedAccount;
     }
 }
 
