b874981b69f672259d2be18ab792f6882c5ae71f
DeyanSpasovBerlin <pislpe@gmail.com>
2025-02-25 19:08:10 +0100

    Client <-> Manager

--- src/main/java/de/telran/bankapp/service/ManagerService.java
+++ src/main/java/de/telran/bankapp/service/ManagerService.java
@@ -1,5 +1,6 @@
 package de.telran.bankapp.service;
 
+import de.telran.bankapp.dto.ClientDto;
 import de.telran.bankapp.entity.Client;
 import de.telran.bankapp.dto.ManagerCreateDto;
 import de.telran.bankapp.dto.ManagerDto;
@@ -52,6 +53,10 @@ public class ManagerService {
         List<Manager> managers = repository.findByFirstName(firstName);
         return mapper.entityListToDto(managers);
     }
+    /*
+    corect query in Postman:
+    get: http://localhost:8080/manager/search?firstName=PeterNew
+    */
 
     public  List<ManagerDto> findByFirstNameAndLastName(String firstName,String lastName){
         List<Manager> managers = repository.findByFirstNameAndLastName(firstName,lastName);
@@ -64,13 +69,103 @@ public class ManagerService {
                 firstName,lastName);
         return mapper.entityListToDto(managers);
     }
+    /*
+    corect query in Postman:
+    get: http://localhost:8080/manager/searchByFirstLetterFromFirstNameAndFirstLetterFromLastName?firstName=P&&lastName=W
+     */
+
 
     @Transactional
     public ManagerDto addManager( ManagerCreateDto dto){
         Manager manager = mapper.createDtoToEntity(dto);
         Manager savedManager = repository.save(manager);
+        if(dto.getClientsDto() != null) {
+            List<ClientDto> clientsDto = dto.getClientsDto();
+            for (ClientDto c: clientsDto){
+                Client client = clientMapper.dtoToEntity(c);
+                client.setManager(savedManager);
+                clientRepository.save(client);
+            }
+        }
         return mapper.entityToDto(savedManager);
     }
+    /*
+    corect query in Postman:
+    post: http://localhost:8080/manager
+    {
+    "lastName": "WeberNew",
+    "firstName": "PeterNew",
+    "clientsDto": [
+        {
+            "id": "20980395-20d0-4ea8-8e4b-de2252a028eb",
+            "lastName": "Müller",
+            "firstName": "Anna",
+            "taxCode": "DE987654321",
+            "email": "a.mueller@example.com",
+            "address": "Munich, Germany",
+            "phone": "+49 89 7654321",
+            "status": "INACTIVE"
+        }
+    ]
+}
+N.B.!! [] is very important becouse ManagerCreateDto has field: private List<ClientDto> clientsDto;
+which in JSON is [] !!!
+     */
+
+    public ManagerDto updateManager(ManagerDto managerDto) {
+        Long id = managerDto.getId();
+        Optional<Manager> optional = repository.findById(id);
+        if (optional.isPresent()) {
+            Manager existingManager = optional.get();
+            existingManager.setFirstName(managerDto.getFirstName());
+            existingManager.setLastName(managerDto.getLastName());
+            existingManager.setStatus(managerDto.getStatus());
+            Manager savedManager = repository.save(existingManager);
+            if(managerDto.getClientsDto() != null) {
+                List<ClientDto> clientsDto = managerDto.getClientsDto();
+                for (ClientDto c: clientsDto){
+                    Client client = clientMapper.dtoToEntity(c);
+                    client.setManager(savedManager);
+                    clientRepository.save(client);
+                }
+            }
+            return mapper.entityToDto(savedManager);
+        }
+        throw new BankAppResourceNotFoundException("Manager with id = " + id + " not found in database");
+    }
+    /*
+    corect query in Postman:
+    put: http://localhost:8080/manager
+    {"id": 11,
+    "lastName": "WeberNewNew",
+    "firstName": "PeterNewOld",
+    "status": "ACTIVE",
+    "clientsDto": [
+        {
+            "id": "20980395-20d0-4ea8-8e4b-de2252a028eb",
+            "lastName": "Müller",
+            "firstName": "Anna",
+            "taxCode": "DE987654321",
+            "email": "a.mueller@example.com",
+            "address": "Munich, Germany",
+            "phone": "+49 89 7654321",
+            "status": "INACTIVE"
+        },
+        {
+                "id": "b03dbcfc-d047-49a7-acbb-f3b1329e1fee",
+                "lastName": "Fischer",
+                "firstName": "Lukas",
+                "taxCode": "DE234567890",
+                "email": "l.fischer@example.com",
+                "address": "Stuttgart, Germany",
+                "phone": "+49 711 9876543",
+                "status": "INACTIVE",
+                "managerId": 3
+            }
+    ]
+}
+     */
+
 
     @Transactional
     public ManagerDto updateManager(ManagerDto manager) {
