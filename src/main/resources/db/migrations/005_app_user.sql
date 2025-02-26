bd4ec01017c39f29140275786112771ee530f0b0
Yaartur10 <yaartur1001@gmail.com>
2025-02-26 22:36:31 +0100

    AppUser-task4

--- src/main/resources/db/migrations/005_app_user.sql
+++ src/main/resources/db/migrations/005_app_user.sql
@@ -1,11 +1,11 @@
 INSERT INTO app_user (id, email, password, role, client_id) VALUES
-                                                     ('1', 'john.doe@example.com', 'password123', 2,'20980395-20d0-4ea8-8e4b-de2252a028eb'),
-                                                     ('2', 'jane.smith@example.com', 'password456', 0, '3d18c400-a7b3-4bb1-bb29-8f566a43eca1'),
-                                                     ('3', 'alice.johnson@example.com', 'password789', 1,'5e659d3c-3925-457d-ae41-ca69001fb11c'),
-                                                     ('4', 'michael.brown@example.com', 'password321', 0, '7d1388c1-04df-4473-b15b-fe614594aa3c'),
-                                                     ('5', 'lisa.white@example.com', 'password654', 1, '9cd1b6c5-06d7-4f6c-9959-3856b1b51045'),
-                                                     ('6', 'david.green@example.com', 'password987', 2, 'b03dbcfc-d047-49a7-acbb-f3b1329e1fee'),
-                                                     ('7', 'susan.black@example.com', 'password159', 0, 'b2c2e8dd-6bce-4401-bd71-ffaed9d6ada0'),
-                                                     ('8', 'robert.blue@example.com', 'password753', 1, 'bb10eec7-e408-4f11-9603-ec3c4a371512'),
-                                                     ('9', 'emily.red@example.com', 'password963', 0, 'd07ec73f-702a-479c-a930-f51596d9b899'),
-                                                     ('10', 'charles.purple@example.com', 'password741', 2, 'f6bc3ef4-4894-42f2-9c7b-66b9a8b80c7c');
+                                                     ('1', 'john.doe@example.com', 'password123', 'CLIENT','20980395-20d0-4ea8-8e4b-de2252a028eb'),
+                                                     ('2', 'jane.smith@example.com', 'password456', 'MANAGER', '3d18c400-a7b3-4bb1-bb29-8f566a43eca1'),
+                                                     ('3', 'alice.johnson@example.com', 'password789', 'ADMIN','5e659d3c-3925-457d-ae41-ca69001fb11c'),
+                                                     ('4', 'michael.brown@example.com', 'password321', 'MANAGER', '7d1388c1-04df-4473-b15b-fe614594aa3c'),
+                                                     ('5', 'lisa.white@example.com', 'password654', 'CLIENT', '9cd1b6c5-06d7-4f6c-9959-3856b1b51045'),
+                                                     ('6', 'david.green@example.com', 'password987', 'MANAGER', 'b03dbcfc-d047-49a7-acbb-f3b1329e1fee'),
+                                                     ('7', 'susan.black@example.com', 'password159', 'ADMIN', 'b2c2e8dd-6bce-4401-bd71-ffaed9d6ada0'),
+                                                     ('8', 'robert.blue@example.com', 'password753', 'CLIENT', 'bb10eec7-e408-4f11-9603-ec3c4a371512'),
+                                                     ('9', 'emily.red@example.com', 'password963', 'MANAGER', 'd07ec73f-702a-479c-a930-f51596d9b899'),
+                                                     ('10', 'charles.purple@example.com', 'password741', 'CLIENT', 'f6bc3ef4-4894-42f2-9c7b-66b9a8b80c7c');
