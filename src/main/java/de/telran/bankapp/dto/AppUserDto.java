f97a52d85751433d3e8ccf8eac8d15869b4db097
Yaartur10 <yaartur1001@gmail.com>
2025-02-27 23:20:22 +0100

    AppUser-task7

--- src/main/java/de/telran/bankapp/dto/AppUserDto.java
+++ src/main/java/de/telran/bankapp/dto/AppUserDto.java
@@ -1,21 +1,14 @@
 package de.telran.bankapp.dto;
 
 import de.telran.bankapp.entity.enums.UserRole;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class AppUserDto {
     private String id;
     private String email;
     private String password;
     private UserRole role;
-
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public UserRole getRole() { return role; }
-    public void setRole(UserRole role) { this.role = role; }
-
-
 }
