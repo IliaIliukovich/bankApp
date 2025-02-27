9412e849c4a03c808b86ad4c557e97d56b2a6c5f
Yaartur10 <yaartur1001@gmail.com>
2025-02-27 22:57:41 +0100

    AppUser-task6

--- src/main/java/de/telran/bankapp/entity/AppUser.java
+++ src/main/java/de/telran/bankapp/entity/AppUser.java
@@ -28,13 +28,4 @@ public class AppUser {
 
   @Enumerated(EnumType.STRING)
   private UserRole role;
-
-  public String getId() { return id; }
-  public void setId(String id) { this.id = id; }
-  public String getEmail() { return email; }
-  public void setEmail(String email) { this.email = email; }
-  public String getPassword() { return password; }
-  public void setPassword(String password) { this.password = password; }
-  public UserRole getRole() { return role; }
-  public void setRole(UserRole role) { this.role = role; }
 }
