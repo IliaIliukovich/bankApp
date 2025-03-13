a5bf4d9317db4d0621815e85f8e29b25a15c9714
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2025-03-13 21:48:30 +0100

    Logger aspect improved

--- src/main/java/de/telran/bankapp/aspect/LogAspect.java
+++ src/main/java/de/telran/bankapp/aspect/LogAspect.java
@@ -2,11 +2,10 @@ package de.telran.bankapp.aspect;
 
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Log4j2
@@ -17,25 +16,33 @@ public class LogAspect {
     public void methodExecuting() {
     }
 
+    @Before("methodExecuting()")
+    public void beforeAdvice(JoinPoint joinPoint) {
+        log.debug("Method {} from class {} started with args:{}",
+                () -> joinPoint.getSignature().getName(),
+                () -> joinPoint.getSourceLocation().getWithinType().getName(),
+                () -> Arrays.toString(joinPoint.getArgs()));
+    }
+
     @AfterReturning(value = "methodExecuting()", returning = "returningValue")
     public void recordSuccessfulExecution(JoinPoint joinPoint, Object returningValue) {
         if (returningValue != null) {
-            log.debug("Успешно выполнен метод - {}, класса- {}, с результатом выполнения - {}\n",
-                    joinPoint.getSignature().getName(),
-                    joinPoint.getSourceLocation().getWithinType().getName(),
-                    returningValue);
+            log.debug("Method {} from class {} completed with return value: {}",
+                    () -> joinPoint.getSignature().getName(),
+                    () -> joinPoint.getSourceLocation().getWithinType().getName(),
+                    () -> returningValue);
         } else {
-            log.debug("Успешно выполнен метод - {}, класса- {}\n",
-                    joinPoint.getSignature().getName(),
-                    joinPoint.getSourceLocation().getWithinType().getName());
+            log.debug("Method {} from class {} completed",
+                    () -> joinPoint.getSignature().getName(),
+                    () -> joinPoint.getSourceLocation().getWithinType().getName());
         }
     }
 
     @AfterThrowing(value = "methodExecuting()", throwing = "exception")
     public void recordFailedExecution(JoinPoint joinPoint, Exception exception) {
-        log.debug("Метод - {}, класса- {}, был аварийно завершен с исключением - {}\n",
-                joinPoint.getSignature().getName(),
-                joinPoint.getSourceLocation().getWithinType().getName(),
-                exception);
+        log.debug("Method {} from class {} produced exception: {}",
+                () -> joinPoint.getSignature().getName(),
+                () -> joinPoint.getSourceLocation().getWithinType().getName(),
+                () -> exception);
     }
 }
