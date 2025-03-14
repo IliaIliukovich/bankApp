55c86b9ec7e5f925e973e13d2d10adb955ebee66
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2025-03-14 10:47:05 +0100

    Scheduling added

--- src/main/java/de/telran/bankapp/BankAppApplication.java
+++ src/main/java/de/telran/bankapp/BankAppApplication.java
@@ -2,8 +2,12 @@ package de.telran.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling
+//@EnableAsync
 public class BankAppApplication {
 
     public static void main(String[] args) {
