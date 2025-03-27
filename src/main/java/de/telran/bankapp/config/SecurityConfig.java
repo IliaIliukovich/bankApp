a2ea96276c8a3c7200bc4187d6c06526a29e141c
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2025-03-27 10:26:17 +0100

    Spring security with in memory users

---
+++ src/main/java/de/telran/bankapp/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package de.telran.bankapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("tom")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}password")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+}
